9a818c52d7e365e8fd37949a62d8290372684add
Salman Ahmad <86913182+salAhamad@users.noreply.github.com>
2021-09-13 18:04:54 +0400

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import countryData from './countries.json'
+import stateData from './states.json'
+import cityData from './cities.json'
+
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [countries, setCountries] = useState(countryData)
+  const [states, setStates] = useState(stateData)
+  const [cities, Cities] = useState(cityData)
+
+  const stateHandler = (e) => {
+    alert()
+  }
+  const selectChangeData = (e) => {
+    console.log(e.target.value);
+  }
+
+
+  return <section className="mainSection p-5 d-flex justify-content-center">
+    <div className="fieldContainer">
+      <div className="row">
+        <div className="col-4">
+          <select className="form-select" aria-label="Default select example" onChange={(e) => selectChangeData(e)}>
+            <option>Select Country</option>
+            { countries.map((country, index) => {
+              return <option id={country.id} value={country.name} key={country.id} onClick={() => stateHandler(country.id)}>{country.name}</option>
+            })}
+          </select>
+        </div>        
+        <div className="col-4">
+          <select className="form-select" aria-label="Default select example">
+            <option>Select States</option>
+            {states.map((state, index) => <option value={state.state_code} key={state.id}>{state.name}</option> )}
+          </select>
+        </div>        
+        <div className="col-4">
+          <select className="form-select" aria-label="Default select example">
+            <option>Open this select menu</option>
+            <option defaultValue="1">One</option>
+            <option defaultValue="2">Two</option>
+            <option defaultValue="3">Three</option>
+          </select>
+        </div>        
+      </div>
     </div>
-  );
+  </section>
 }
 
 export default App;
